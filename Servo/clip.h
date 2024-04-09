3c5a758016bd2daa5af86863c01239854806370b
Gitama911XYZ <123150468+Gitama911XYZ@users.noreply.github.com>
2024-04-09 16:25:52 +0800

    Update and rename onoff.h to clip.h

--- Servo/clip.h
+++ Servo/clip.h
@@ -1,5 +1,5 @@
-#ifndef _ONOFF_H
-#define _ONOFF_H
+#ifndef _CLIP_H
+#define _CLIP_H
 
 #include <mbed.h>
 #include "Adafruit_PWMServoDriver.h"
@@ -7,18 +7,22 @@
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 
-class onoff {
+class clip {
 private:
     I2C* _i2c; // 儲存I2C對象的指針
     Adafruit_PWMServoDriver _pwmServo; // PWM servo driver實例
 
 public:
-onoff(I2C* i2c, uint8_t addr); // 構造函數，接受I2C對象和地址
+clip(I2C* i2c, uint8_t addr); // 構造函數，接受I2C對象和地址
 void setup();
 // void access(I2C* abc);
 void mainservo(int servonum, bool isopen );
 void Lservo(int servonum, bool isopen);
 void Rservo(int servonum, bool isopen);
+void Set1(bool isopen);
+void Set2(bool isopen);
+void OpenAll();
+void CloseAll();
 
 
 };
