b1b14b34fb231200f547402ae1e47c184513d99e
Gitama911XYZ <123150468+Gitama911XYZ@users.noreply.github.com>
2024-06-11 13:59:50 +0800

    Update clip.h

--- Servo/clip.h
+++ Servo/clip.h
@@ -1,9 +1,8 @@
 #ifndef _CLIP_H
 #define _CLIP_H
-
+#include <map.h>
 #include <mbed.h>
 #include "Adafruit_PWMServoDriver.h"
-
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 
@@ -11,9 +10,15 @@ class clip {
 private:
     I2C* _i2c; // 儲存I2C對象的指針
     Adafruit_PWMServoDriver _pwmServo; // PWM servo driver實例
+    Map map;
+    PwmOut& UP;
+    PwmOut& Down;
+    Timeout timeout;
+
+    
 
 public:
-clip(I2C* i2c, uint8_t addr); // 構造函數，接受I2C對象和地址
+clip(I2C* i2c, uint8_t addr, PwmOut& up, PwmOut& down); // 構造函數，接受I2C對象和地址
 void setup();
 // void access(I2C* abc);
 void mainservo(int servonum, bool isopen );
@@ -23,6 +28,11 @@ void Set1(bool isopen);
 void Set2(bool isopen);
 void OpenAll();
 void CloseAll();
+void setDegree(int servonum, int degree);
+void stopUP();
+void stopDown();
+void  PushRod(bool UpDown);
+
 
 
 };
